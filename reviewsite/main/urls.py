6af2c913ed0b0af7a768009fb4d73981dacdfdd0
l <l>
2021-06-07 18:33:47 +0500

    register, login, logout

--- reviewsite/main/urls.py
+++ reviewsite/main/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
-    path('login', views.login),
-    path('set-up', views.authorization),
-    path('movies', views.movies),
-    path('movies/the-woman-in-the-window', views.movie_example),
+    path('', views.index, name='home'),
+    path('login', views.login, name='login'),
+    path('set-up', views.set_up, name='set up'),
+    path('movies', views.movies, name='movies'),
+    path('movies/the-woman-in-the-window', views.movie_example, name='the-woman-in-the-window'),
+    path('register', views.register, name='register'),
+    path('loginPage', views.loginPage, name='loginPage'),
+    path('logout', views.logoutUser, name='logout'),
 ]
--- reviewsite/main/views.py
+++ reviewsite/main/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, logout, login as dj_login
+from django.contrib import messages
 
 from .models import Movie
 
@@ -15,12 +19,52 @@ def index(request):
     return render(request, 'main/index.html')
 
 
-def login(request):
-    return render(request, 'main/login.html')
-
-
-def authorization(request):
+def set_up(request):
     return render(request, 'main/Set Up.html')
+
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'account/register.html', context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'main/login.html', context)
+
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            dj_login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+
+    context = {}
+    return render(request, 'account/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('loginPage')
 
 
 def movies(request):
